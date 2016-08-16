5ec5ce5e5a105a6d64e19ae600c4f2937c811203
Daniel Fernandez-Pihl <daniel.fernandez-pihl@ericsson.com>
2016-08-16 14:49:39 +0200

    copyspecial exercise

--- python/google-python-exercises/copyspecial/copyspecial.py
+++ python/google-python-exercises/copyspecial/copyspecial.py
@@ -18,7 +18,33 @@ import commands
 # +++your code here+++
 # Write functions and modify main() to call them
 
+def get_special_paths(dirname):
+  files_absolute = []
+  filenames = os.listdir(dirname)
+  for filename in filenames:
+    match = re.search(r'__\w+__', filename)
+    if match:
+      filename_path = os.path.join(dirname, filename)
+      filename_abs_path = os.path.abspath(filename_path)
+      files_absolute.append(filename_abs_path)
 
+  return files_absolute
+
+def copy_to(paths, dirname):
+  dest = os.path.abspath(dirname)
+  if not os.path.exists(dest):
+    os.makedirs(dest)
+
+  for path in paths:
+    shutil.copy(path, dest)
+
+def zip_to(paths, zipfile):
+  zip_cmd = 'zip -j ' + zipfile + ' ' + ' '.join(paths)
+  (status, output) = commands.getstatusoutput(zip_cmd)
+  if status:
+    sys.stderr.write('zip error status: ' + str(status) + '\n')
+    sys.stderr.write(output + '\n')
+    sys.exit(1)
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -51,5 +77,27 @@ def main():
   # +++your code here+++
   # Call your functions
   
+  all_special_files = []
+  for arg in args:
+    all_special_files += get_special_paths(arg)
+   
+  #check for duplicates
+  base_names = []
+  for special_file in all_special_files:
+    base_name = os.path.basename(special_file)
+    if base_name in base_names:
+      sys.stderr.write('filename: ' + base_name + ' already exist!\n')
+      sys.exit(1)
+    else:
+      base_names.append(base_name)
+
+  if todir:
+    copy_to(all_special_files, todir)
+  elif tozip:
+    zip_to(all_special_files, tozip)
+  else:
+    for special_file in all_special_files:
+      print special_file
+
 if __name__ == "__main__":
   main()
