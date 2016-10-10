f2857ec689c5061959027395b26eec3055e11671
daniel <daniel@linux.com>
2016-10-10 08:58:18 +0200

    Bowling, using roll_list with foldl

--- haskell/Bowling/Bowling.hs
+++ haskell/Bowling/Bowling.hs
@@ -1,5 +1,22 @@
-module Bowling(add1) where
+module Bowling(roll, score, Frame) where
 
-add1 :: Int -> Int
-add1 x = x + 1
+type Frame = (Int, Int, Int) --second field is -1 on strike, third field present in last frame (if spare or strike)
+
+roll :: [Frame] -> Int -> [Frame]
+roll [] n = [(n, -1, -1)]
+roll frames n = reverse $ rollReversed (reverse frames)
+        where rollReversed rframes
+                | length rframes == 10 = let (x,y,z) = head rframes in if y == -1 then (x,n,-1):(tail rframes) else if x + y >= 10 then (x,y,n):(tail rframes) else rframes
+                | ((10,-1,-1):fs) <- rframes = (n,-1,-1):rframes
+                | ((x,-1,-1):fs) <- rframes = (x,n,-1):fs
+                | ((x,y,-1):fs) <- rframes = (n,-1,-1):rframes
+
+score :: [Frame] -> Int
+score [] = 0
+score [(x,-1,-1)] = x
+score [(x,y,-1)] = x + y
+score [(x,y,z)] = x + y + z
+score ((10,-1,-1):(10,-1,-1):(x3,y3,z3):xs) = 10 + 10 + x3 + (score $ (10,-1,-1):(x3,y3,z3):xs)
+score ((10,-1,-1):(x2,y2,z2):xs) = 10 + x2 + y2 + (score $ (x2,y2,z2):xs)
+score ((x,y,-1):(x2,y2,z2):xs) = if x + y == 10 then 10 + x2 + (score $ (x2,y2,z2):xs) else x + y + (score $ (x2,y2,z2):xs)
 
--- haskell/Bowling/Bowling_Test.hs
+++ haskell/Bowling/Bowling_Test.hs
@@ -3,9 +3,29 @@ module Bowling_Test where
 import Bowling
 import Test.HUnit
 
-test_add1 = TestCase $ assertEqual "add1 on 1 should be 2" 2 (add1 1)
+roll_list :: [Frame] -> [Int] -> [Frame]
+roll_list frames = foldl roll frames
 
+--roll tests
+test_roll_first = TestCase $ assertEqual "roll a 1, first roll" [(1, -1, -1)] (roll [] 1)
+test_roll_two = TestCase $ assertEqual "roll a 1, then a 2" [(1, 2, -1)] (roll_list [] [1,2])
+test_roll_strike = TestCase $ assertEqual "roll a strike, then a 2" [(10, -1, -1), (2, -1, -1)] (roll_list [] [10,2])
+test_roll_all_zeroes = TestCase $ assertEqual "roll 20 zeroes" (replicate 10 (0,0,-1)) (roll_list [] $ replicate 20 0)
+test_roll_all_ones = TestCase $ assertEqual "roll 20 ones" (replicate 10 (1,1,-1)) (roll_list [] $ replicate 20 1)
+test_roll_all_strike = TestCase $ assertEqual "roll 12 tens" ((replicate 9 (10,-1,-1)) ++ [(10,10,10)]) (roll_list [] $ replicate 12 10)
 
-addTests = TestList [test_add1]
+--TODO bonus roll tests (see python solution)
 
-main = runTestTT $ TestList [addTests]
+--score tests
+test_score_no_rolls = TestCase $ assertEqual "score should be 0 after no rolls" 0 (score [])
+test_score_one_roll = TestCase $ assertEqual "score should be 2 after one roll of 2" 2 (score $ roll [] 2)
+test_score_all_zeroes = TestCase $ assertEqual "score should be 0 after 20 zero rolls" 0 (score $ roll_list [] $ replicate 20 0)
+test_score_all_ones = TestCase $ assertEqual "score should be 20 after 20 one rolls" 20 (score $ roll_list [] $ replicate 20 1)
+test_score_all_spare = TestCase $ assertEqual "score should be 150 after 21 five rolls" 150 (score $ roll_list [] $ replicate 21 5)
+test_score_all_strike = TestCase $ assertEqual "score should be 300 after 12 ten rolls" 300 (score $ roll_list [] $ replicate 12 10)
+test_score_custom = TestCase $ assertEqual "score should be 133 after custom series" 133 (score $ roll_list [] [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6])
+
+rollTests = TestList [test_roll_first, test_roll_two, test_roll_strike, test_roll_all_zeroes, test_roll_all_ones, test_roll_all_strike]
+scoreTests = TestList [test_score_no_rolls, test_score_one_roll, test_score_all_zeroes, test_score_all_ones, test_score_all_spare, test_score_all_strike, test_score_custom]
+
+main = runTestTT $ TestList [rollTests, scoreTests]
